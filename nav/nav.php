4159ce5d710a6e67176982f77e135b42d6b1cc88
Glen Donn B. Alpasan <62196184+glentooth00@users.noreply.github.com>
2023-01-15 11:10:16 +0800

    first commit

---
+++ nav/nav.php
@@ -0,0 +1,27 @@
+<nav class="nav">
+    <ul>
+        <li>
+            <a href="#">HOME</a>
+        </li>
+        <li>
+            <a href="#about">ABOUT</a>
+        </li>
+        <li>
+            <a href="#">PROJECTS</a>
+        </li>
+        <li>
+            <a href="#">CONTACT</a>
+        </li>
+        <li>
+            <a href="#about">ABOUT</a>
+        </li>
+     
+       
+           
+        
+        
+    </ul>   
+
+    <span><img src="assets/images/icons/logo.png" id="logo"></span>
+
+</nav>
